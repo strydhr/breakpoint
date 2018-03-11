c25aea6b680b2f8aff467df88d64aaa5d61f3c86
Satyia Anand <satyia@Satyias-iMac.local>
2018-03-11 17:04:02 +0800

    adding groupfeed

---
+++ breakpoint/breakpoint/Model/Group.swift
@@ -0,0 +1,9 @@
+//
+//  Group.swift
+//  breakpoint
+//
+//  Created by Satyia Anand on 11/03/2018.
+//  Copyright © 2018 Satyia Anand. All rights reserved.
+//
+
+import Foundation
---
+++ breakpoint/breakpoint/View/GroupFeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupFeedCell.swift
+//  breakpoint
+//
+//  Created by Satyia Anand on 11/03/2018.
+//  Copyright © 2018 Satyia Anand. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
